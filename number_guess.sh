9845f8e32575987aacd9973b1770703c64950c69
dandi ans <dandianastasa12@gmail.com>
2024-08-01 10:02:15 +0000

    chore: modify the comment text to get the user's id

--- number_guess.sh
+++ number_guess.sh
@@ -5,5 +5,5 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo "Enter your username:"
 read USERNAME
 
-# check whether it is exist or not
+# perform a query, get the user's id by its username
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
