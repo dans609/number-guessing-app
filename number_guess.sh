8ad70f96b7de328d8e9a0a03359abf4baa090ad8
dandi ans <dandianastasa12@gmail.com>
2024-08-01 09:55:14 +0000

    feat: get the user's id y its username

--- number_guess.sh
+++ number_guess.sh
@@ -4,3 +4,6 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo "Enter your username:"
 read USERNAME
+
+# check whether it is exist or not
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
