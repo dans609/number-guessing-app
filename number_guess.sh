ff2df62e9542ad4c2304e8041376a7b95d0e4089
dandi ans <dandianastasa12@gmail.com>
2024-08-01 10:16:32 +0000

    feat: add MAIN function, and move all logic to the function body

--- number_guess.sh
+++ number_guess.sh
@@ -2,16 +2,20 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-echo "Enter your username:"
-read USERNAME
+MAIN() {
+  echo "Enter your username:"
+  read USERNAME
 
-# perform a query, get the user's id by its username
-USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+  # perform a query, get the user's id by its username
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 
-# check whether the user is exist or not
-if [[ -z $USER_ID ]]; then
-  INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')" 2>/dev/null)
-  if [[ ! $INSERT_USER = "INSERT 0 1" ]]; then
-    echo -e "\nusername is too long, maximum characters is (22)"    
+  # check whether the user is exist or not
+  if [[ -z $USER_ID ]]; then
+    INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')" 2>/dev/null)
+    if [[ ! $INSERT_USER = "INSERT 0 1" ]]; then
+      echo -e "\nusername is too long, maximum characters is (22)"    
+    fi
   fi
-fi
+}
+
+MAIN
