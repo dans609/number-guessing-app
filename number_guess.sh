e1f6fa0b5b50bb7903697feded138cdd97e83638
dandi ans <dandianastasa12@gmail.com>
2024-08-01 10:13:07 +0000

    feat: print error message when it's fail to insert

--- number_guess.sh
+++ number_guess.sh
@@ -11,4 +11,7 @@ USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 # check whether the user is exist or not
 if [[ -z $USER_ID ]]; then
   INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')" 2>/dev/null)
+  if [[ ! $INSERT_USER = "INSERT 0 1" ]]; then
+    echo -e "\nusername is too long, maximum characters is (22)"    
+  fi
 fi
